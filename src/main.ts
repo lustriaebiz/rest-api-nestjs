0542d35fcb0b7fc449cdc2fef26f6291106a112d
lustria ebiz <lustriaebiz@gmail.com>
2020-02-04 11:33:08 +0700

    swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+/** swagger */
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  setupswagger(app);
+
   await app.listen(3000);
 }
+
+function setupswagger(app){
+  const options = new DocumentBuilder()
+    .setTitle('VCN API')
+    .setDescription('Virtual Card Number by BNI')
+    .setVersion('1.0')
+    .addTag('vcn')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('docs-api', app, document);
+}
+
 bootstrap();
